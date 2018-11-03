c2081fd8f42d1d0880a17ec02000017e89146c14
gamijin0 <497386856@qq.com>
2018-11-03 12:55:39 +0800

    finish the whole new framework;

--- cache.py
+++ cache.py
@@ -1,10 +1,43 @@
 import hashlib
+import math
+
+
+# 向云服务查询一条最短路径
+def mockCloudBaseQuery(query: "(Node,Node)") -> "Path":
+    return Path([])
+
+
+# 从长的path中截取subpath作为query的答案
+# 由于path不在Cache2中，所以不能用Rtree加速
+def extractSubPath(path: "Path", query: "(Node,Node)") -> "Path":
+    return Path([])
 
 
 # PSA
-# 大量query内存缩减
-def PathSharing():
-    pass
+# 大量query的内部覆盖缩减
+def PSA(querylist: "list([(Node,Node),])"):
+    querylist.sort(key=lambda x: x[0].lengthTo(x[1]), reverse=True)
+
+    i = 0
+    j = 1
+
+    res = {}
+
+    while (i < len(querylist) - 1):
+        longerPath = mockCloudBaseQuery(querylist[i])
+        res[querylist[i]] = longerPath
+        while (j < len(querylist)):
+            shorterQuery = querylist[j]
+            if (longerPath.isCoverNode(shorterQuery[0])
+                and longerPath.isCoverNode(shorterQuery[1])):
+                print("PSA Hit:%s-->%s" % (longerPath, shorterQuery))
+                res[shorterQuery] = extractSubPath(longerPath, shorterQuery)
+                querylist.remove(shorterQuery)
+            else:
+                j += 1
+        i += 1
+
+    return res
 
 
 class Node:
@@ -20,6 +53,9 @@ class Node:
             return True
         return False
 
+    def lengthTo(self, a_node: "Node"):
+        return math.sqrt((self.x - a_node.x) ** 2 + (self.y - a_node.y) ** 2)
+
     def __str__(self):
         return "Node<%d>(%.2f,%.2f)" % (self.nid, self.x, self.y)
 
@@ -29,9 +65,15 @@ class Node:
 class Path:
     shareAbility = 0
     nodelist = []
+    length = 0.0
+    nodeNumber = 0
 
     def __init__(self, nodelist):
         self.nodelist = nodelist
+        L2 = (self.originNode.x - self.destinationNode.x) ** 2 + \
+             (self.originNode.y - self.destinationNode.y) ** 2  # 适当放大避免精度问题
+        self.length = math.sqrt(L2 * (10 ** 8))  # 适当放大避免精度问题
+        self.nodeNumber = len(nodelist)
 
     # 起点
     @property
@@ -44,16 +86,16 @@ class Path:
         return self.nodelist[-1]
 
     # path是否包含一个node
-    def isContainNode(self, node: list):
+    def isCoverNode(self, node: "Node"):
         for n in self.nodelist:
             if (node == n):
                 return True
         return False
 
     # 是否包含了另一个path
-    def isCover(self, path2: "Path"):
-        if (self.isContainNode(path2.originNode)
-            and self.isContainNode(path2.destinationNode)
+    def isCoverPath(self, path2: "Path"):
+        if (self.isCoverNode(path2.originNode)
+            and self.isCoverNode(path2.destinationNode)
             ):
             return True
         return False
@@ -64,8 +106,11 @@ class Path:
         return "-".join([str(node.nid) for node in self.nodelist])
         # return int(hashlib.sha256(str(self.nodelist).encode('utf-8')).hexdigest(), 16) % 10 ** 9
 
+    @property
+    def ShareAblityPerNode(self):
+        return self.shareAbility * 1.0 / self.nodeNumber
 
-# 第一种缓存结构
+
 # 所有的查找都是遍历，没有索引加速
 # Cache[
 #   Path[
@@ -74,12 +119,48 @@ class Path:
 #   ...]
 class PathCache1:
     pathlist = []
+    capacity = 0
+    size = 0
 
-    def __init__(self):
+    def __init__(self, capacity=1000):
+        self.capacity = capacity
         pass
 
     def addpath(self, path: "Path"):
         self.pathlist.append(path)
+        self.size += len(path.nodelist)
+
+    # 输入一些path来构造缓存
+    def PCCA(self, pathlist: "[Path]", ):
+
+        pathlist.sort(key=lambda x: x.length, reverse=True)
+
+        i = 0
+        j = 1
+
+        while (i < len(pathlist) - 1):
+            longerOne = pathlist[i]
+            while (j < len(pathlist)):
+                shorterOne = pathlist[j]
+                if (longerOne.isCoverPath(shorterOne)):
+                    longerOne.shareAbility += 1.0
+                    shorterOne.shareAbility = 0
+                    pathlist.remove(shorterOne)
+                else:
+                    j += 1
+            i += 1
+
+        pathlist.sort(key=lambda x: x.ShareAblityPerNode, reverse=True)
+
+        for path in pathlist:
+            if (self.size + path.nodeNumber > self.capacity):
+                break
+            else:
+                self.addpath(path)
+
+    def PCA(self, querylist: "[(Node,Node)]"):
+        pass
+
 
 
 # 第二种缓存结构
@@ -87,8 +168,11 @@ class PathCache1:
 class PathCache2:
     __reverseDict = {}
     __neighbourDict = {}
+    capacity = 0
+    size = 0
 
-    def __int__(self):
+    def __int__(self, capacity=1000):
+        self.capacity = capacity
         pass
 
     def __insertNodeReversePathTable(self, node, path: "Path"):
@@ -108,3 +192,32 @@ class PathCache2:
         for node in path.nodelist:
             self.__insertNodeReversePathTable(node, path)
         self.__updateNeighbourTable(path)
+        self.size += path.nodeNumber
+
+    def PCCA(self, pathlist: "list[Path]"):
+
+        pathlist.sort(key=lambda x: x.length, reverse=True)
+
+        i = 0
+        j = 1
+
+        while (i < len(pathlist) - 1):
+            longerOne = pathlist[i]
+            while (j < len(pathlist)):
+                shorterOne = pathlist[j]
+
+                if (longerOne.isCoverPath(shorterOne)):
+                    longerOne.shareAbility += 1.0
+                    shorterOne.shareAbility = 0
+                    pathlist.remove(shorterOne)
+                else:
+                    j += 1
+            i += 1
+
+        pathlist.sort(key=lambda x: x.ShareAblityPerNode, reverse=True)
+
+        for path in pathlist:
+            if (self.size + path.nodeNumber > self.capacity):
+                break
+            else:
+                self.addpath(path)
